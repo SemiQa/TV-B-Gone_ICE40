a7a7078c179ad856a0f961a5b407f5e6f66e9972
PS-SemiQa <201234504+PS-SemiQa@users.noreply.github.com>
2025-09-03 00:12:15 +0200

    Add possible output forcing when pwm generator is disabled

--- src/pwm_generator.sv
+++ src/pwm_generator.sv
@@ -14,24 +14,27 @@ module pwm_generator
 
     input   bit  reset_in,      // resets counter and output when driven high (synchronous)
     input   bit  enable_in,     // PWM generated when high
+    input   bit  forced_in,     // output state to be forced when not enabled
 
     input   bit  [WIDTH-1:0] compare_value_in,   // PWM period in counts
     input   bit  update_comp_value_in,           // write enable, active high (synchronous)
 
-    output  reg  pwm_out        // PWM output
+    output  bit  pwm_out        // PWM output
 );
 
 reg [WIDTH-1:0] counter_r;
 reg [WIDTH-1:0] ocr_r;
 
+reg out;
+
 always @(posedge clock_in) begin
     if (reset_in) begin
         counter_r <= 0;
         ocr_r <= 0;
-        pwm_out <= 0;
+        out <= 0;
     end else if (enable_in) begin
         if (counter_r == ocr_r) begin
-            pwm_out <= ~pwm_out;
+            out <= ~pwm_out;
             counter_r <= 0;
         end else begin
             counter_r <= counter_r + 1;
@@ -41,5 +44,7 @@ always @(posedge clock_in) begin
         counter_r <= 0;
     end
 end
+
+assign pwm_out = enable_in ? out : forced_in;
 
 endmodule
